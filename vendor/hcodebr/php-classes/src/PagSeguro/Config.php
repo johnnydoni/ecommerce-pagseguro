0cea036868d05e85750b73ead9ee3e3e6c19f602
johnnydoni <inods@hotmail.com>
2018-05-29 11:56:23 -0300

    Order na Session e Payment

--- vendor/hcodebr/php-classes/src/PagSeguro/Config.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Config.php
@@ -15,6 +15,9 @@ class Config {
     const SANDBOX_SESSIONS = "https://ws.sandbox.pagseguro.uol.com.br/v2/sessions";
     const PRODUCTION_SESSIONS = "https://ws.pagseguro.uol.com.br/v2/sessions";
 
+    const SANDBOX_URL_JS = "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+    const PRODUCTION_URL_JS = "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+
     public static function getAuthentication():array {
         if (Config::SANDBOX === true) {
             return [
@@ -31,6 +34,10 @@ class Config {
 
     public static function getUrlSessions():string {
         return (Config::SANDBOX === true) ? Config::SANDBOX_SESSIONS : PRODUCTION_SESSIONS;
+    }
+
+    public static function getUrlJS() {
+        return (Config::SANDBOX === true) ? Config::SANDBOX_URL_JS : PRODUCTION_URL_JS;   
     }
 }
 
