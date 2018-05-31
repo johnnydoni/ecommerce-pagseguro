e846f990fc294a475cf3d33406da1fbb9ae10715
johnnydoni <inods@hotmail.com>
2018-05-31 11:06:38 -0300

    Obter métodos de pagamento

--- views-cache/header.b4cd273e040f5bc82adcddfa782defe1.rtpl.php
+++ views-cache/header.b4cd273e040f5bc82adcddfa782defe1.rtpl.php
@@ -3,23 +3,24 @@
     Hcode Store by hcode.com.br
 -->
 <html lang="pt-br">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hcode Store</title>
-    
+
     <!-- Google Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    
+
     <!-- Bootstrap -->
     <link rel="stylesheet" href="/res/site/css/bootstrap.min.css">
-    
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/res/site/css/font-awesome.min.css">
-    
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="/res/site/css/owl.carousel.css">
     <link rel="stylesheet" href="/res/site/css/style.css">
@@ -31,9 +32,13 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
-  <body>
-   
+    <script>
+        window.scripts = [];
+    </script>
+</head>
+
+<body>
+
     <div class="header-area">
         <div class="container">
             <div class="row">
@@ -55,7 +60,7 @@
                         </ul>
                     </div>
                 </div>
-                
+
                 <div class="col-md-4">
                     <div class="header-right">
                         <ul class="list-unstyled list-inline">
@@ -80,17 +85,20 @@
                 </div>
             </div>
         </div>
-    </div> <!-- End header area -->
-    
+    </div>
+    <!-- End header area -->
+
     <div class="site-branding-area">
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
                     <div class="logo">
-                        <h1><a href="/"><img src="/res/site/img/logo.png"></a></h1>
+                        <h1>
+                            <a href="/"><img src="/res/site/img/logo.png"></a>
+                        </h1>
                     </div>
                 </div>
-                
+
                 <div class="col-sm-6">
                     <div class="shopping-item">
                         <a href="/cart">Carrinho - <span class="cart-amunt">R$<?php echo getCartVlSubTotal(); ?></span> <i class="fa fa-shopping-cart"></i> <span class="product-count"><?php echo getCartNrQtd(); ?></span></a>
@@ -98,8 +106,9 @@
                 </div>
             </div>
         </div>
-    </div> <!-- End site branding area -->
-    
+    </div>
+    <!-- End site branding area -->
+
     <div class="mainmenu-area">
         <div class="container">
             <div class="row">
@@ -110,14 +119,15 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                </div> 
+                </div>
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="/">Home</a></li>
                         <li><a href="#">Produtos</a></li>
                         <li><a href="/cart">Carrinho</a></li>
                     </ul>
-                </div>  
+                </div>
             </div>
         </div>
-    </div> <!-- End mainmenu area -->
+    </div>
+    <!-- End mainmenu area -->
