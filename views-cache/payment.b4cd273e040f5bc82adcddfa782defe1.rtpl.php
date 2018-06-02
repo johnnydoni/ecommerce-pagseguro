14de0a6a437d5ba49201c68fbc83a4316e73afaf
johnnydoni <inods@hotmail.com>
2018-06-02 08:30:16 -0300

    Obteve a bandeira do cartão

--- views-cache/payment.b4cd273e040f5bc82adcddfa782defe1.rtpl.php
+++ views-cache/payment.b4cd273e040f5bc82adcddfa782defe1.rtpl.php
@@ -280,8 +280,6 @@
         PagSeguroDirectPayment.getPaymentMethods({
             amount: parseFloat("<?php echo htmlspecialchars( $order["vltotal"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"),
             success: function(response) {
-                console.log(response);
-
                 var tplDebit = Handlebars.compile($("#tpl-payment-debit").html());
                 var tplCredit = Handlebars.compile($("#tpl-payment-credit").html());
 
@@ -318,6 +316,28 @@
             },
             complete: function(response) {
 
+            }
+        });
+
+        $("#number_field").on("change", function() {
+            var value = $(this).val();
+            if (value.length >= 6) {
+                PagSeguroDirectPayment.getBrand({
+                    cardBin: value.substring(0, 6),
+                    success: function(response) {
+                        console.log(response);
+                    },
+                    error: function(response) {
+                        var errors = [];
+                        for (var code in response.errors) {
+                            errors.push(response.errors[code]);
+                        }
+                        showError(errors.toString());
+                    },
+                    complete: function(response) {
+                        //tratamento comum para todas chamadas
+                    }
+                });
             }
         });
     });
