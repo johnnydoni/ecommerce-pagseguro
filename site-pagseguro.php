d4eaed2f2bb668aa2e2b8acf3ce6e8a2ff7c20b9
johnnydoni <inods@hotmail.com>
2018-06-30 20:34:03 -0300

    Pagamento Debito

--- site-pagseguro.php
+++ site-pagseguro.php
@@ -15,6 +15,19 @@ use \Hcode\PagSeguro\CreditCard\Installment;
 use \Hcode\PagSeguro\CreditCard;
 use \Hcode\PagSeguro\Item;
 use \Hcode\PagSeguro\Payment;
+use \Hcode\PagSeguro\Bank;
+
+$app->get("/payment/success/debit", function() {
+	User::verifyLogin(false);
+	$order = new Order();
+	$order->getFromSession();
+	$order->get((int)$order->getidorder());
+	$page = new Page();
+	$page->setTpl("payment-success-debit", [
+		"order"=>$order->getValues()
+	]);
+
+});
 
 $app->get("/payment/success/boleto", function() {
 	User::verifyLogin(false);
@@ -37,6 +50,55 @@ $app->get("/payment/success", function() {
 		"order"=>$order->getValues()
 	]);
 
+});
+
+$app->post("/payment/debit", function(){
+	User::verifyLogin(false);
+	$order = new Order();
+	$order->getFromSession();
+	$order->get((int)$order->getidorder());
+	$address = $order->getAddress();
+	$cart = $order->getCart();
+	$cpf = new Document(Document::CPF, $_POST['cpf']);
+	$phone = new Phone($_POST['ddd'], $_POST['phone']);
+
+	$shippingAddress = new Address(
+		$address->getdesaddress(),
+		$address->getdesnumber(),
+		$address->getdescomplement(),
+		$address->getdesdistrict(),
+		$address->getdeszipcode(),
+		$address->getdescity(),
+		$address->getdesstate(),
+		$address->getdescountry()		
+	);
+
+	$birthDate = new DateTime($_POST['birth']);
+	$sender = new Sender($order->getdesperson(), $cpf, $birthDate, $phone, $order->getdesemail(), $_POST['hash']);
+
+	$shipping = new Shipping($shippingAddress, (float)$cart->getvlfreight(), Shipping::PAC);
+
+	$payment = new Payment($order->getidorder(), $sender, $shipping);
+
+	foreach ($cart->getProducts() as $product) {
+		$item = new Item(
+			(int)$product["idproduct"],
+			$product["desproduct"],
+			(float)$product["vlprice"],
+			(int)$product["nrqtd"]
+		);
+		$payment->addItem($item);
+	}
+
+	$bank = new Bank($_POST["bank"]);
+	$payment->setBank($bank);
+
+	Transporter::sendTransaction($payment);	
+
+	echo json_encode([
+		"success"=>true
+	]);
+	
 });
 
 $app->post("/payment/boleto", function(){
